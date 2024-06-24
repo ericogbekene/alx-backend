644a723d9554e5d317037d397c2f580c74d11ccb
Eric Ogbekene <ericogbekene@icloud.com>
2024-06-24 23:50:34 +0100

    a simple pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -3,6 +3,17 @@ import math
 from typing import List
 
 
+def index_range(page: int, page_size: int) -> tuple:
+    """
+    """
+    if page == 1:
+        new_tuple = tuple((0, (page_size * page)))
+        return new_tuple
+    start_idx = ((page - 1) * page_size)
+    end_idx = (start_idx + page_size)
+    return tuple((start_idx, end_idx))
+
+
 class Server:
     """Server class to paginate a database of popular baby names.
     """
@@ -23,5 +34,8 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-            pass
-            
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+        dataset = self.dataset()
+        slice_range = index_range(page, page_size)
+        return dataset[slice_range[0]:slice_range[1]]
